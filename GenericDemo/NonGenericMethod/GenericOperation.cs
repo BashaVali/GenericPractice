6911beba7e0f51d2300e48f063c995ed5f01df65
BashaVali <bashavali222@gmail.com>
2022-12-14 05:53:45 +0530

    Non Generic Method

--- GenericDemo/NonGenericMethod/GenericOperation.cs
+++ GenericDemo/NonGenericMethod/GenericOperation.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GenericDemo
+namespace GenericDemo.NonGenericMethod
 {
     public class GenericOperation
     {
--- GenericDemo/Program.cs
+++ GenericDemo/Program.cs
@@ -1,41 +1,38 @@
-﻿using System;
+﻿using GenericDemo.NonGenericMethod;
 
-namespace GenericDemo
+class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        Console.WriteLine("Non Generic Method");
+        bool flag = true;
+        while (flag)
         {
-            Console.WriteLine("Genereic & Non Generic Method");
-            bool flag = true;
-            while (flag)
-            {  
 
 
-                Console.WriteLine("***Select Options:*** \n1.IntArray \n2.DoubleArr \n3.CharArr \n2.Exit");
-                Console.Write("\nEnter your choice: ");
-                int choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
-                {
-                    case 1:
-                        GenericOperation generic = new GenericOperation();
-                        int[] intArr = { 1, 2, 3, 4, 5 };
-                        generic.ToPrintArray(intArr);
-                        break;
-                    case 2:
-                        GenericOperation generic1 = new GenericOperation();
-                        double[] doubleArr = { 1.1, 2.2, 3.3, 4.4, 5.5 };
-                        generic1.ToPrintArray(doubleArr);
-                        break;
-                    case 3:
-                        GenericOperation generic2 = new GenericOperation();
-                        char[] charArr = { 'a', 'b', 'c', 'd', 'e' };
-                        generic2.ToPrintArray(charArr);
-                        break;
-                    case 4:
-                        flag = false;
-                        break;
-                }
+            Console.WriteLine("***Select Options:*** \n1.IntArray \n2.DoubleArray  \n3.CharArray  \n4.Exit");
+            Console.Write("\nEnter your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    GenericOperation generic = new GenericOperation();
+                    int[] intArr = { 1, 2, 3, 4, 5 };
+                    generic.ToPrintArray(intArr);
+                    break;
+                case 2:
+                    GenericOperation generic1 = new GenericOperation();
+                    double[] doubleArr = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+                    generic1.ToPrintArray(doubleArr);
+                    break;
+                case 3:
+                    GenericOperation generic2 = new GenericOperation();
+                    char[] charArr = { 'a', 'b', 'c', 'd', 'e' };
+                    generic2.ToPrintArray(charArr);
+                    break;
+                case 4:
+                    flag = false;
+                    break;
             }
         }
     }
