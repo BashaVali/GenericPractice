b42b85ffa68e1774237ac042a954a702502f88f7
BashaVali <bashavali222@gmail.com>
2022-12-14 05:37:29 +0530

    Non Generic Method

---
+++ GenericDemo/GenericOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class GenericOperation
+    {
+        public void ToPrintArray(int[] intArr)
+        {
+            foreach (var data in intArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        public void ToPrintArray(double[] doubleArr)
+        {
+            foreach (var data in doubleArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        public void ToPrintArray(char[] charArr)
+        {
+            foreach (var data in charArr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
---
+++ GenericDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GenericDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Genereic & Non Generic Method");
+            bool flag = true;
+            while (flag)
+            {  
+
+
+                Console.WriteLine("***Select Options:*** \n1.IntArray \n2.DoubleArr \n3.CharArr \n2.Exit");
+                Console.Write("\nEnter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        GenericOperation generic = new GenericOperation();
+                        int[] intArr = { 1, 2, 3, 4, 5 };
+                        generic.ToPrintArray(intArr);
+                        break;
+                    case 2:
+                        GenericOperation generic1 = new GenericOperation();
+                        double[] doubleArr = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+                        generic1.ToPrintArray(doubleArr);
+                        break;
+                    case 3:
+                        GenericOperation generic2 = new GenericOperation();
+                        char[] charArr = { 'a', 'b', 'c', 'd', 'e' };
+                        generic2.ToPrintArray(charArr);
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
